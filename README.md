b9f207f2f85064f2126004ebe9e871da7c9c55c0
Mark <mdarens@gmail.com>
2018-09-06 17:01:38 -0500

    Initial: works with Prince + opinionated changes
    - Don't alter the tree of the targeted elements as it could throw off eg flex layouts
      Instead just change the parent id & throw when exists.
    - Add more scope exemptions (page, charset, etc)
    - Remove call to parentElement as it's not supported. Use parentNode always
    - Always use DOMContentLoaded
    - Remove minified copy
    - Update readme

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # Scoper
 
-A polyfill for scoped HTML style elements.
-
-* [Demo](http://jsfiddle.net/thomaspark/rpe402LL/)
+A polyfill for scoped HTML style elements, updated to work with [Prince](http://princexml.com/).
 
 # Quick Start
 
 Just include `scoper.js` on your page to start using scoped HTML style elements.
 
-Install via [npm](https://www.npmjs.com/package/css-scoper): `npm install css-scoper`
-
 # How It Works
 
-Each `<style scoped>` element's parent is wrapped with a `span` that is given a unique ID. Its rules are moved to `head` and all selectors prepended with their respective ID.
+Each `<style scoped>` element's parent is given a unique ID. Its rules are moved to `head` and all selectors prepended with their respective ID.
 
 Scoper is only active when the page contains scoped styles and the browser doesn't support them.
 
--- scoper.js
+++ scoper.js
@@ -1,131 +1,129 @@
-/* global exports */
+// modified from https://github.com/thomaspark/scoper
 
 function scoperInit() {
-  var style = document.createElement("style");
-  style.appendChild(document.createTextNode(""));
-  document.head.appendChild(style);
-  style.sheet.insertRule("body { visibility: hidden; }", 0);
+	var style = document.createElement("style");
+	style.appendChild(document.createTextNode(""));
+	document.head.appendChild(style);
 }
 
 function scoper(css, prefix) {
-  var re = new RegExp("([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)", "g");
-  css = css.replace(re, function(g0, g1, g2) {
+	var re = new RegExp("([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)", "g");
+	css = css.replace(re, function (g0, g1, g2) {
 
-    if (g1.match(/^\s*(@media|@.*keyframes|to|from|@font-face|1?[0-9]?[0-9])/)) {
-      return g1 + g2;
-    }
+		if (g1.match(/^\s*(@charset|@document|@import|@media|@namespace|@page|@supports|@.*keyframes|to|from|@font-face|1?[0-9]?[0-9])/)) {
+			return g1 + g2;
+		}
 
-    if (g1.match(/:scope/)) {
-      g1 = g1.replace(/([^\s]*):scope/, function(h0, h1) {
-        if (h1 === "") {
-          return "> *";
-        } else {
-          return "> " + h1;
-        }
-      });
-    }
+		if (g1.match(/:scope/)) {
+			g1 = g1.replace(/([^\s]*):scope/, function (h0, h1) {
+				if (h1 === "") {
+					return "> *";
+				} else {
+					return "> " + h1;
+				}
+			});
+		}
 
-    g1 = g1.replace(/^(\s*)/, "$1" + prefix + " ");
+		g1 = g1.replace(/^(\s*)/, "$1" + prefix + " ");
 
-    return g1 + g2;
-  });
-  
-  return css;
+		return g1 + g2;
+	});
+
+	return css;
 }
 
 function scoperProcess() {
-  var styles = document.body.querySelectorAll("style[scoped]");
+	var styles = document.body.querySelectorAll("style[scoped]");
 
-  if (styles.length === 0) {
-    document.getElementsByTagName("body")[0].style.visibility = "visible";
-    return;
-  }
+	if (styles.length === 0) {
+		return;
+	}
 
-  var head = document.head || document.getElementsByTagName("head")[0];
+	var head = document.head || document.getElementsByTagName("head")[0];
 
-  for (var i = 0; i < styles.length; i++) {
-    var style = styles[i];
-    var css = style.innerHTML;
+	for (var i = 0; i < styles.length; i++) {
+		var style = styles[i];
+		var css = style.innerHTML;
 
-    if (css && (style.parentElement.nodeName !== "BODY")) {
-      var id = "scoper-" + i;
-      var prefix = "#" + id;
+		if (css && (style && style.parentNode.nodeName !== "BODY")) {
+			var id = "scoper-" + i;
+			var prefix = "#" + id;
 
-      var wrapper = document.createElement("span");
-      wrapper.id = id;
+			// var wrapper = document.createElement("span");
+			// wrapper.id = id;
 
-      var parent = style.parentNode;
-      var grandparent = parent.parentNode;
+			var parent = style.parentNode;
+			var grandparent = parent.parentNode;
 
-      grandparent.replaceChild(wrapper, parent);
-      wrapper.appendChild(parent);
-      style.parentNode.removeChild(style);
+			if (parent.id) {
+				throw new Error(
+					"Scoped style #" + id + " is attempting to overwrite already existing id \"" +
+					parent.id +
+					"\" on its parent." + 
+					"\n" +
+					"Only use `<style scoped>` as a descendant of nodes without existing id attributes.");
+			}
 
-      var newstyle = document.createElement("style");
-      newstyle.setAttribute('data-scoped-style-for', id);
-      var csses = scoper(css, prefix);
-      if (newstyle.styleSheet){
-          newstyle.styleSheet.cssText = csses;
-      } else {
-          newstyle.appendChild(document.createTextNode(csses));
-      }
+			parent.id = id;
+			style.parentNode.removeChild(style);
 
-      head.appendChild(newstyle);
-    }
-  }
+			var newstyle = document.createElement("style");
+			newstyle.setAttribute('data-scoped-style-for', id);
+			var csses = scoper(css, prefix);
+			if (newstyle.styleSheet) {
+				newstyle.styleSheet.cssText = csses;
+			} else {
+				newstyle.appendChild(document.createTextNode(csses));
+			}
 
-  document.getElementsByTagName("body")[0].style.visibility = "visible";
+			head.appendChild(newstyle);
+		}
+	}
+
+	document.getElementsByTagName("body")[0].style.visibility = "visible";
 }
 
 function scoperReset() {
-  var styles = document.head.querySelectorAll("style[data-scoped-style-for]");
-  for (var i = 0; i < styles.length; i++) {
-    var style = styles[i];
-    var wrapperElementId = style.getAttribute('data-scoped-style-for');
-    var wrapperEl = document.getElementById(wrapperElementId);
-    if(wrapperEl) { // Node may have disappeared, in that case nothing should happen
-      var css = style.innerHTML;
-      var resettedCss = css.replace("#"+wrapperElementId+" ", "");
+	var styles = document.head.querySelectorAll("style[data-scoped-style-for]");
+	for (var i = 0; i < styles.length; i++) {
+		var style = styles[i];
+		var wrapperElementId = style.getAttribute('data-scoped-style-for');
+		var wrapperEl = document.getElementById(wrapperElementId);
+		if (wrapperEl) { // Node may have disappeared, in that case nothing should happen
+			var css = style.innerHTML;
+			var resettedCss = css.replace("#" + wrapperElementId + " ", "");
 
-      var parent = wrapperEl.parentNode;
-      var targetEl = wrapperEl.childNodes[0];
+			var parent = wrapperEl.parentNode;
+			var targetEl = wrapperEl.childNodes[0];
 
-      parent.replaceChild(targetEl, wrapperEl);
-      var scopedStyle = document.createElement("style");
-      scopedStyle.setAttribute("scoped", "true");
-      if (scopedStyle.styleSheet){
-        scopedStyle.styleSheet.cssText = resettedCss;
-      } else {
-        scopedStyle.appendChild(document.createTextNode(resettedCss));
-      }
-      targetEl.appendChild(scopedStyle);
-    }
+			parent.replaceChild(targetEl, wrapperEl);
+			var scopedStyle = document.createElement("style");
+			scopedStyle.setAttribute("scoped", "true");
+			if (scopedStyle.styleSheet) {
+				scopedStyle.styleSheet.cssText = resettedCss;
+			} else {
+				scopedStyle.appendChild(document.createTextNode(resettedCss));
+			}
+			targetEl.appendChild(scopedStyle);
+		}
 
-    style.parentNode.removeChild(style);
-  }
+		style.parentNode.removeChild(style);
+	}
 }
 
 function scoperRestart() {
-  scoperReset();
-  scoperProcess();
+	scoperReset();
+	scoperProcess();
 }
 
-(function() {
-  "use strict";
+(function () {
+	"use strict";
 
-  if (typeof document === "undefined" || ("scoped" in document.createElement("style"))) {
-    return;
-  }
-  
-  scoperInit();
+	if (typeof document === "undefined" || ("scoped" in document.createElement("style"))) {
+		return;
+	}
 
-  if (document.readyState === "complete" || document.readyState === "loaded") {
-    scoperProcess();
-  } else {
-    document.addEventListener("DOMContentLoaded", scoperProcess);
-  }
+	scoperInit();
+
+	document.addEventListener("DOMContentLoaded", scoperProcess);
 }());
-
-if (typeof exports !== "undefined") {
-  exports.scoper = scoper;
-}
